bc0ce93630147007e704c1ea03e5ee68af911a9c
nabil <nabil.boudouch@gmail.com>
2016-09-30 10:29:12 +0100

    create entity Article

---
+++ src/FTV/ApiBundle/Entity/Article.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace FTV\ApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Article
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="FTV\ApiBundle\Entity\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=150)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="_leading", type="text", nullable=true)
+     */
+    private $leading;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="body", type="text", nullable=true)
+     */
+    private $body;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     * @Gedmo\Timestampable(on="create")
+     */
+    private $createdAt;
+
+    /**
+     * @var string
+     * @ORM\Column(name="createdBy", type="text")
+     */
+    private $createdBy;
+
+    /**
+     * @Gedmo\Slug(fields={"title"})
+     * @ORM\Column(unique=true)
+     */
+
+    private $slug;
+
+    /**
+     * @return mixed
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Article
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set leading
+     *
+     * @param string $leading
+     * @return Article
+     */
+    public function setLeading($leading)
+    {
+        $this->leading = $leading;
+
+        return $this;
+    }
+
+    /**
+     * Get leading
+     *
+     * @return string
+     */
+    public function getLeading()
+    {
+        return $this->leading;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     * @return Article
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Article
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set createdBy
+     *
+     * @param string $createdBy
+     * @return Article
+     */
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return string
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+}
---
+++ src/FTV/ApiBundle/FTVApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FTV\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FTVApiBundle extends Bundle
+{
+}
