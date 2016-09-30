a014947e226d762ff23d3c1c57b488b92b899577
nabil <nabil.boudouch@gmail.com>
2016-09-30 10:55:13 +0100

    added Gedmo annotations and form crud for tests

---
+++ src/FTV/ApiBundle/Form/ArticleType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace FTV\ApiBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ArticleType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('leading')
+            ->add('body')
+            ->add('createdBy')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'FTV\ApiBundle\Entity\Article'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ftv_apibundle_article';
+    }
+}
