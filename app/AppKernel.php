1420d850fd56628aeb32a019d7047c2469747faa
nabil <nabil.boudouch@gmail.com>
2016-09-30 12:10:47 +0100

    added bazinga and pagerfanta for listArticles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new FTV\ApiBundle\FTVApiBundle(),
+            new Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle(),
+            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
 
 
         );
